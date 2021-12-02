86c79b584b0821f26ad455d140db3f9cd32571eb
fabiha-fabs <lipu.scse@gmail.com>
2021-12-03 01:17:17 +0600

    Project initialized

---
+++ src/main/java/com/fabs/base64decoder/Base64decoderApplication.java
@@ -0,0 +1,13 @@
+package com.fabs.base64decoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Base64decoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Base64decoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabs/base64decoder/Base64decoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabs.base64decoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Base64decoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
