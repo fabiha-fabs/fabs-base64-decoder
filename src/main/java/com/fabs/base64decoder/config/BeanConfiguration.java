e67355b50467d04d86622c52220c29e396b241e1
fabiha-fabs <lipu.scse@gmail.com>
2021-12-03 12:23:09 +0600

    Eureka client config done

--- src/main/java/com/fabs/base64decoder/config/BeanConfiguration.java
+++ src/main/java/com/fabs/base64decoder/config/BeanConfiguration.java
@@ -1,6 +1,7 @@
 package com.fabs.base64decoder.config;
 
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfiguration {
 
 	@Bean
+	@LoadBalanced
 	public Base64 getBase64() {
 		return new Base64();
 	}
